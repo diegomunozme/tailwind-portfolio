a3863f9c3be6f482de902e2febcef951d8cd3b89
diegomunozme <diego.munoz.me@gmail.com>
2022-09-08 06:09:54 -0700

    First project with tailwind, personal portfolio

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="bg-transparent z-2 text-center justify-betwee flex flex-wrap px-20 place-content-center">
+      <div className="flex flex-row px- w-1/2 place-content-center">
+        <h1 className="flex-auto font-mono text-xl font-bold hover:underline w-1">Diego Munoz</h1>
+        <div className="flex-auto hover:underline w-1">Experience</div>
+        <div className="flex-auto hover:underline w-1">Posts</div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
